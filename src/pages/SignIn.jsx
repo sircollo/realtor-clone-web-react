82b8e1220c3f5e4acfdcfecac3cac7d17efb4576
sircollo <sircollo71@gmail.com>
2023-01-08 11:59:53 +0300

    Feat: adds sign in with email and password functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react'
 import {AiFillEyeInvisible, AiFillEye} from 'react-icons/ai'
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import OAuth from '../components/OAuth';
+import { signInWithEmailAndPassword, getAuth } from 'firebase/auth';
+import { toast } from 'react-toastify';
 export default function SignIn() {
   const [formData, setFormData] = useState({
     email:"",
@@ -16,6 +18,20 @@ export default function SignIn() {
     }))
   }
   const [showPassword, setShowPassword] = useState(false);
+  const navigate = useNavigate();
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth();
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      if(userCredential.user){
+        toast.success("login successful")
+        navigate("/")
+      }
+    } catch (error) {
+      toast.error("Invalid credentials")
+    }
+  }
   return (
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
@@ -24,7 +40,7 @@ export default function SignIn() {
           <img src='https://images.unsplash.com/flagged/photo-1564767609342-620cb19b2357?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8a2V5fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='sign-in-image' className='w-full rounded-2xl'/>
         </div>
         <div className='w-full md:w-[67%]  lg:w-[40%] lg:ml-20'>
-          <form>
+          <form onSubmit={onSubmit}>
             <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out mb-6' type="email" id="email" value={email} onChange={onChange} placeholder="Email Address"/>
             <div className='relative mb-6'>
             <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out' type={showPassword ? "text": "password"} id="password" value={password} onChange={onChange} placeholder="Password"/>
