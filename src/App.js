63a7de223f84b55958040126c6462113b3d67c69
sircollo <sircollo71@gmail.com>
2023-01-15 01:04:39 +0300

    create listing page and use swiper to create image slider

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import EditListing from './pages/EditListing';
+import Listing from "./pages/Listing";
 function App() {
   return (
     <>
@@ -32,6 +33,7 @@ function App() {
           <Route  path="/edit-listing" element={<PrivateRoute/>}>
             <Route path="/edit-listing/:listingid" element={<EditListing />} />
           </Route>
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
         </Routes>
       </Router>
       <ToastContainer
