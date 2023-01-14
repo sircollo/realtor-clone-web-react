7f315a931151ed2bc85314f7b752346975110fc4
sircollo <sircollo71@gmail.com>
2023-01-15 01:31:40 +0300

    Feat: add share link copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -6,10 +6,12 @@ import { db } from '../firebase';
 import { Swiper, SwiperSlide } from "swiper/react"
 import SwiperCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper"
 import "swiper/css/bundle"
+import {FaShare} from "react-icons/fa"
 export default function Listing() {
     SwiperCore.use([Autoplay, Navigation, Pagination])
     const [listing, setListing] = useState()
     const [loading, setLoading] = useState(true)
+    const [shareLinkCopied, setShareLinkCopied] = useState(false)
     const params = useParams()
     useEffect(()=>{
         async function fetchListing(){
@@ -41,6 +43,17 @@ export default function Listing() {
                 </SwiperSlide>
             ))}
         </Swiper>
+        <div className='fixed top-[13%] right-[3%] z-50 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex items-center justify-center' onClick={()=>{
+            navigator.clipboard.writeText(window.location.href)
+            setShareLinkCopied(true)
+            setTimeout(()=>{
+                setShareLinkCopied(false)
+            }, 2000)
+            }}>
+            <FaShare className='text-lg text-slate-500'/>
+        </div>
+        {shareLinkCopied && <p className="fixed top-[23%] right-[2%] font-semibold border-2 border-gray-400 rounded-md z-50 bg-white p-1">Link copied!</p>}
     </main>
   )
 }
+// onClick={()=>(navigator.clipboard.writeText(window.location.href))}
